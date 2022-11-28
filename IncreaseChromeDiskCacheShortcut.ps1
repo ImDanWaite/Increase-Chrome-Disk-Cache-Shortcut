fd5844579566e42a7506c027519754e52e8e5677
ImDanWaite <103328674+ImDanWaite@users.noreply.github.com>
2022-11-28 14:21:45 +0000

    Add files via upload

---
+++ IncreaseChromeDiskCacheShortcut.ps1
@@ -0,0 +1,9 @@
+﻿$WshShell = New-Object -ComObject 'WScript.Shell'
+$Shortcut = $WshShell.CreateShortcut("$env:public\desktop\Chrome.lnk")
+#The below can be amended based on the location of Chrome in program files - e.g. c:\program files\google\chrome\application\chrome.exe
+$Shortcut.TargetPath = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+#The below --disk-cache-size command increases the disk cache size.
+#The below --new-window opens Chrome in a new window with it's default start-up page as www.google.co.uk
+$Shortcut.Arguments = "--disk-cache-size=1073741824 --new-window https://www.google.co.uk/"
+$Shortcut.WorkingDirectory = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+$Shortcut.Save()
